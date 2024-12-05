49b87c93c2e620777000928391c68499299f8165
Manuel Fabri <manuelnfabri@gmail.com>
2024-12-05 10:59:10 +0100

    add missing protocol on vercel branch url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   publicRuntimeConfig: {
-    baseUrl: process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_BRANCH_URL || 'https://expensapp.manuelfabri.com',
+    baseUrl:
+      process.env.NEXT_PUBLIC_BASE_URL ||
+      `https://${process.env.VERCEL_BRANCH_URL}` ||
+      'https://expensapp.manuelfabri.com',
   },
 };
 
