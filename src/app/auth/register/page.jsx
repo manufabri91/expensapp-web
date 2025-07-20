d577afb067a8460d9a8dce8bd0c7ae6aa0f855be
Manuel Fabri <manuelnfabri@gmail.com>
2025-07-20 20:48:29 +0200

    fix(login): fix login/register redirection

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,14 +1,9 @@
 import { LoginForm } from '@/components/LoginForm';
-import { useRouter } from 'next/navigation';
 
 const RegisterPage = () => {
-  const router = useRouter();
-  const handleSuccess = () => {
-    router.push('/dashboard');
-  };
   return (
     <div className="flex">
-      <LoginForm mode="register" callback={handleSuccess} />
+      <LoginForm mode="register" />
     </div>
   );
 };
